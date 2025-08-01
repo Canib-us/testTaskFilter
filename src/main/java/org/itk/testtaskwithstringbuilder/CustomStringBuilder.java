56b48af89a1e79a936f02c0a9369bd7bdcd6b08d
Canib-us <162473942+Canib-us@users.noreply.github.com>
2025-08-01 11:56:35 +0700

    ArrayUtil.java

--- src/main/java/org/itk/testtaskwithstringbuilder/CustomStringBuilder.java
+++ src/main/java/org/itk/testtaskwithstringbuilder/CustomStringBuilder.java
@@ -1,4 +1,22 @@
 package org.itk.testtaskwithstringbuilder;
 
-public class CustomStringBuilder {
+import java.lang.reflect.Array;
+
+public class ArrayUtil {
+    @SuppressWarnings("unchecked")
+    public static <T> T[] filter(T[] array, Filter<T> filter) {
+        if (array == null) {
+            return null;
+        }
+        T[] filteredArray = (T[]) Array.newInstance(array.getClass().getComponentType(), array.length);
+        for (int i = 0; i < array.length; i++) {
+            filteredArray[i] = filter.apply(array[i]);
+        }
+        return filteredArray;
+    }
+
+}
+
+interface Filter<T>{
+    T apply(T o);
 }
